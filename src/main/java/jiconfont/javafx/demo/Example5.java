9a4db4030c948f09cdd2c8a090177bda60057d49
cadu <cadu@localhost.localdomain>
2016-02-04 21:05:07 -0200

    Adding project files

--- src/main/java/jiconfont/javafx/demo/Example5.java
+++ src/main/java/jiconfont/javafx/demo/Example5.java
@@ -1,10 +1,18 @@
 package jiconfont.javafx.demo;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import jiconfont.icons.*;
-import jiconfont.javafx.CatalogPane;
+import jiconfont.icons.Elusive;
+import jiconfont.icons.FontAwesome;
+import jiconfont.javafx.IconFontFX;
+import jiconfont.javafx.IconNode;
+import jiconfont.javafx.StackedIconNode;
 
 /**
  * Copyright (c) 2016 jIconFont <BR>
@@ -27,28 +35,31 @@ import jiconfont.javafx.CatalogPane;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-public class ShowCatalogFX extends Application {
+public class Example5 extends Application {
 
     @Override
     public void start(Stage stage) {
-        CatalogPane catalogPane = new CatalogPane();
-        for (FontAwesome icon : FontAwesome.values()) {
-            catalogPane.register(icon);
-        }
-        for (GoogleMaterialDesignIcons icon : GoogleMaterialDesignIcons.values()) {
-            catalogPane.register(icon);
-        }
-        for (Iconic icon : Iconic.values()) {
-            catalogPane.register(icon);
-        }
-        for (Elusive icon : Elusive.values()) {
-            catalogPane.register(icon);
-        }
-        for (Entypo icon : Entypo.values()) {
-            catalogPane.register(icon);
-        }
-        catalogPane.update();
-        Scene scene = new Scene(catalogPane, 950, 650);
+        StackPane root = new StackPane();
+        root.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+
+        // Register the IconFonts
+        IconFontFX.register(FontAwesome.getIconFont());
+        IconFontFX.register(Elusive.getIconFont());
+
+        IconNode iconNode1 = new IconNode(FontAwesome.DATABASE);
+        iconNode1.setIconSize(26);
+        iconNode1.setFill(Color.BLACK);
+
+        IconNode iconNode2 = new IconNode(Elusive.REMOVE);
+        iconNode2.setIconSize(14);
+        iconNode2.setFill(Color.RED);
+        iconNode2.setStroke(Color.WHITE);
+
+        StackedIconNode stackedIconNode = new StackedIconNode(iconNode1);
+        stackedIconNode.add(Pos.BOTTOM_LEFT, iconNode2);
+
+        root.getChildren().add(stackedIconNode);
+        Scene scene = new Scene(root, 300, 300);
         stage.setTitle("jIconFont - JavaFX - Catalog");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/jiconfont/javafx/demo/Example6.java
@@ -0,0 +1,62 @@
+package jiconfont.javafx.demo;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import jiconfont.icons.FontAwesome;
+import jiconfont.javafx.IconFontFX;
+import jiconfont.javafx.IconNode;
+
+/**
+ * Copyright (c) 2016 jIconFont <BR>
+ * <BR>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:<BR>
+ * <BR>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.<BR>
+ * <BR>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+public class Example6 extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        StackPane root = new StackPane();
+        root.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+
+        // Register the IconFont
+        IconFontFX.register(FontAwesome.getIconFont());
+
+        IconNode iconNode = new IconNode();
+        iconNode.getStyleClass().add("myClass");
+
+        root.getChildren().add(iconNode);
+        Scene scene = new Scene(root, 300, 300);
+        scene.getStylesheets().add("style.css");
+        stage.setTitle("jIconFont - JavaFX - Catalog");
+        stage.setScene(scene);
+        stage.show();
+        stage.centerOnScreen();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
